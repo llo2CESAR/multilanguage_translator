2e7782aa65e0895b689bf495e623fb52ba68d130
anaritamoura <ana.rcorreia@hotmail.com>
2025-10-05 19:52:08 -0300

    Adição dos códigos do front

--- Projetofinal_librese/main.py
+++ Projetofinal_librese/main.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from source.infrastructure.persistence.in_memory_libras_repository import InMemoryLibrasRepository
 from source.use_cases.translate_text_to_libras import TranslateTextToLibrasUseCase
 from source.infrastructure.web.translation_controller import translation_bp, TranslationController
 
-#app = Flask(__name__, template_folder='../templates')  # Ajuste o path relativo para templates
 app = Flask(__name__, template_folder="templates")
-
 CORS(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 repository = InMemoryLibrasRepository()
 translate_use_case = TranslateTextToLibrasUseCase(repository)
